837bcc7444c96ee02aadb7188aac3c6a9896cfd7
minju26 <mjalswn26@gmail.com>
2024-03-07 11:42:37 +0900

    [DP]1003.py

--- Algorithm/BOJ/DynamicProgramming/[DP]1003.py
+++ Algorithm/BOJ/DynamicProgramming/[DP]1003.py
@@ -1,2 +1,23 @@
 #boj 1003 :: 피보나치 함수 :: silv.3
 
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+testCase = [int(input()) for _ in range(n)]
+m = max(testCase)
+
+dp_0 = [0]*(m+1)
+dp_1 = [0]*(m+1)
+
+for i in range(m+1):
+    if i == 0:
+        dp_0[i] = 1
+    elif i == 1:
+        dp_1[i] = 1
+    else:
+        dp_0[i] = dp_0[i-1] + dp_0[i-2]
+        dp_1[i] = dp_1[i-1] + dp_1[i-2]
+
+for i in testCase:
+    print(dp_0[i], dp_1[i])
