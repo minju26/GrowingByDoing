543ecb7e40ee94dd5397e5cefb93702cb8d11e6c
minju26 <mjalswn26@gmail.com>
2024-03-11 14:00:34 +0900

    Rename folders

---
+++ Algorithm/BOJ/GraphSearch/[Graph]1260.py
@@ -0,0 +1,52 @@
+#boj 1260 :: DFS와 BFS :: silv.2
+
+import sys
+input = sys.stdin.readline
+
+n, m, s = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+dresult = []
+bresult = []
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    graph[i].append(j)
+    graph[j].append(i)
+
+for i in range(n):
+    graph[i].sort()
+
+
+dvisited = [False]*(n+1)
+def dfs(graph, visited, s):
+    visited[s] = True
+    dresult.append(s)
+
+    for i in graph[s]:
+        if not visited[i]:
+            dfs(graph, visited, i)
+
+
+
+def bfs(graph, s):
+    visited = [False]*(n+1)
+    queue = [s]
+    visited[s] = True
+    bresult.append(s)
+
+    while queue:
+        v = queue.pop(0)
+        
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True 
+                bresult.append(i)
+
+
+dfs(graph, dvisited, s)
+bfs(graph, s)
+
+print(*dresult)
+print(*bresult)
